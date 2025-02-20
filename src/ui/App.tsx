c51b1033927e4533f50eddee9bb991a45ddd5d29
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-02-19 22:11:47 -0300

    Tailwind and DaisyUI Configured

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,10 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <div>
+    <div data-theme="customTheme">
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-primary">Primary</button>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,22 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        customTheme: {
+          primary: "#A31D1D",
+          "primary-content": "#6D2323",
+          secondary: "#3F4F44",
+          "secondary-content": "#2C3930",
+          accent: "#9F5255",
+          "accent-content": "#7C444F",
+          neutral: "#E5D0AC",
+          "base-100": "#FEF9E1",
+        },
+      },
+      "dark",
+    ],
+  },
 };
