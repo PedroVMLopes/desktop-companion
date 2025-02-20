a6a0823ec36ab565c689ea9e7d564af4cff8a2bd
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-02-19 22:21:45 -0300

    Color palette changes

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -7,8 +7,10 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-neutral">Neutral</button>
       <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,9 @@ export default {
     themes: [
       {
         customTheme: {
-          primary: "#A31D1D",
-          "primary-content": "#6D2323",
-          secondary: "#3F4F44",
-          "secondary-content": "#2C3930",
-          accent: "#9F5255",
-          "accent-content": "#7C444F",
+          primary: "#6D2323",
+          secondary: "#2C3930",
+          accent: "#3B6790",
           neutral: "#E5D0AC",
           "base-100": "#FEF9E1",
         },
