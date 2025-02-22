8519fce1e0b6458170dfb3d91b078e8a247ed9ab
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-02-22 03:32:21 -0300

    Minor Changes

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -16,10 +16,10 @@ export const Navbar = () => {
 
     return (
         <div className='NavbarRender  bg-base-300 m-1 w-[98%] rounded-xl'>
-            <nav className="flex flex-row items-center w-full max-w-[420px] p-1 px-1">
+            <nav className="flex flex-row items-center w-full max-w-[420px] p-1">
                 <ul className="navbar-nav">
                     <label
-                        className="btn btn-primary bg-opacity-85 swap swap-rotate text-accent text-lg font-Paprika min-h-0 h-min px-[6px] py-[2px] rounded-xl shadow-sm shadow-stone-800 items-center"
+                        className="btn btn-primary bg-opacity-85 swap swap-rotate text-accent text-lg font-Paprika min-h-0 h-min px-[6px] py-[3px] rounded-xl shadow-sm shadow-stone-800 items-center"
                     >
                         {/*<IoMenu />*/}
                         <input type="checkbox" onClick={() => setOpen(!open)}/>
@@ -41,7 +41,7 @@ export const Navbar = () => {
 function NavMenu() {
     return (
         <motion.ul
-            className="flex flex-row w-full justify-evenly items-center"
+            className="flex flex-row w-full items-center"
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.6 }}
@@ -50,7 +50,7 @@ function NavMenu() {
             {menuItems.map(({ Icon, action, dataTip }, index) => (
                 <motion.button
                     key={index}
-                    className="btn btn-neutral tooltip tooltip-bottom min-h-0 h-min min-w-0 p-[6px] text-xl rounded-lg shadow-sm shadow-stone-800"
+                    className="btn btn-neutral tooltip tooltip-bottom min-h-0 h-min min-w-0 p-[6px] text-xl rounded-lg shadow-sm shadow-stone-800 ml-2"
                     initial={{ scale: 0.8, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
                     exit={{ scale: 0.8, opacity: 0 }}
@@ -63,7 +63,7 @@ function NavMenu() {
             ))}
             {/* Theme Toggle Button */}
             <motion.button
-                    className="btn btn-neutral min-h-0 h-min min-w-0 p-[6px] text-xl rounded-lg shadow-sm shadow-stone-800"
+                    className="btn btn-neutral min-h-0 h-min min-w-0 p-[6px] text-xl rounded-lg shadow-sm shadow-stone-800 ml-2"
                     initial={{ scale: 0.8, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
                     exit={{ scale: 0.8, opacity: 0 }}
