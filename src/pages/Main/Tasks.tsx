ccee3edff46710dd937040f12f0a5ba975a670fb
PedroVMLopes <pedrovitormlopes@gmail.com>
2025-02-24 18:20:53 -0300

    Tasks Implementations

--- src/pages/Main/Tasks.tsx
+++ src/pages/Main/Tasks.tsx
@@ -1,41 +1,41 @@
 import { useState } from "react"
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function Tasks() {
-    const [ isAddTaskExpanded, setAddTaskExpanded ] = useState(false);
+    const [ isAddTaskMenuExpanded, setAddTaskMenuExpanded ] = useState(false);
 
-    const handleAddTask = () => {
-        setAddTaskExpanded(false);
+    const handleAddTaskMenu = () => {
+        setAddTaskMenuExpanded(false);
     }
 
     return (
         <div className="TasksMain bg-base-200 rounded-box mt-1 p-1 pb-2">
             <div className="flex flex-row justify-between mt-1 px-2">
                 <h1 className="flex font-Shrikhand text-xl">Tarefas: </h1>
-                <button onClick={() => setAddTaskExpanded(!isAddTaskExpanded)} className="btn btn-sm btn-outline btn-secondary font-bold">Add {isAddTaskExpanded ? "-" : "+"}</button>
+                <button onClick={() => setAddTaskMenuExpanded(!isAddTaskMenuExpanded)} className="btn btn-sm btn-outline btn-secondary font-bold">Add {isAddTaskMenuExpanded ? "-" : "+"}</button>
             </div>
             
-            {isAddTaskExpanded && <AddTask handleAddTask={handleAddTask}/>}
-
-            <Task />
+            <AnimatePresence> {isAddTaskMenuExpanded && <AddTaskMenu handleAddTaskMenu={handleAddTaskMenu}/>} </AnimatePresence>
+            <AnimatePresence> <Task /> </AnimatePresence>
+            
         </div>
     )
 }
 
-function AddTask({ handleAddTask }: { handleAddTask: () => void }) {
+function AddTaskMenu({ handleAddTaskMenu }: { handleAddTaskMenu: () => void }) {
     return (
         <motion.div 
             initial={{ opacity: 0, y: -10 }} 
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 0.2 }}
             className="addTask bg-base-100 rounded-box m-1 mt-2 p-2 pt-2"
         >
             <h1 className="ml-1 mb-1 font-Poppins">Adicione uma nova tarefa:</h1>
             <label className="input input-bordered flex items-center gap-2">
                 Nome
                 <input type="text" className="grow" placeholder="" />
-                <button onClick={handleAddTask} className="btn btn-sm btn-accent">Adicionar</button>
+                <button onClick={handleAddTaskMenu} className="btn btn-sm btn-accent">Adicionar</button>
             </label>
         </motion.div>
     );
@@ -48,9 +48,13 @@ function Task() {
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -10 }}
             transition={{ duration: 0.5 }}
-            className="addTask bg-base-100 rounded-box m-1 mt-2 p-2 pt-2"
+            className="addTask bg-base-100 rounded-box m-1 mt-2 p-2 pl-3"
         >
-            <h1 className="ml-1 mb-1 font-Poppins">Nome da task:</h1>
+            <h1 className="mb-1 font-Poppins font-bold text-accent">Nome da task</h1>
+            <div className="flex flex-row items-center">
+                <h1 className="font-Poppins">Tempo Gasto: </h1>
+                <input type="text" value={"0 horas e 42 min"} readOnly className="bg-base-200 text-secondary font-semibold w-min p-1 ml-1 rounded-md" />
+            </div>
         </motion.div>
     )
 }
